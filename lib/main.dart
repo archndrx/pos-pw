e6a351a2a6c6ef41753fe676d7b5b63f58643c79
Chandra <ardhyachandrax@gmail.com>
2023-10-26 19:00:32 +0700

    init project + setup firebase

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:point_of_sales/view/screen/auth/auth_view_model.dart';
+import 'package:point_of_sales/view/screen/auth/auth_tree.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:point_of_sales/view/screen/cart/cart_view_model.dart';
+import 'package:point_of_sales/view/screen/product/product_view_model.dart';
+import 'package:provider/provider.dart';
+
+Future main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ProductProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => AuthProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => CartProvider(),
+        ),
+      ],
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: AuthTree(),
+      ),
+    );
+  }
+}
