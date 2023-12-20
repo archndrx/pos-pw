30e5561e146daa7a7ff903908f8657f312caf11c
Chandra <ardhyachandrax@gmail.com>
2023-12-20 20:45:00 +0700

    add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:point_of_sales/view/screen/auth/auth_view_model.dart';
 import 'package:point_of_sales/view/screen/auth/auth_tree.dart';
+import 'package:point_of_sales/view/screen/auth/auth_view_model.dart';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:point_of_sales/view/screen/cart/cart_view_model.dart';
 import 'package:point_of_sales/view/screen/product/product_view_model.dart';
+import 'package:point_of_sales/view/screen/sales/sales_provider.dart';
+import 'package:point_of_sales/view/screen/user/user_provider.dart';
 import 'package:provider/provider.dart';
 
 Future main(List<String> args) async {
@@ -27,6 +30,12 @@ class MainApp extends StatelessWidget {
         ),
         ChangeNotifierProvider(
           create: (_) => CartProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => UserProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => SalesProvider(),
         ),
       ],
       child: const MaterialApp(
