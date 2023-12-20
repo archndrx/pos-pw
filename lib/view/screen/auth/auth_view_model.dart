ab96cd1cc101af2a3eb5e81ab32d3f10d89136e2
Chandra <ardhyachandrax@gmail.com>
2023-12-20 20:50:59 +0700

    remove register

--- lib/view/screen/auth/auth_view_model.dart
+++ lib/view/screen/auth/auth_view_model.dart
@@ -1,13 +1,10 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class AuthProvider extends ChangeNotifier {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   User? get currentUser => _firebaseAuth.currentUser;
   Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
-  bool isLogin = true;
 
   Future<void> signInWithEmailAndPassword({
     required String email,
@@ -17,28 +14,7 @@ class AuthProvider extends ChangeNotifier {
         email: email, password: password);
   }
 
-  Future<void> createUserWithEmailAndPassword({
-    required String email,
-    required String password,
-    required String role,
-  }) async {
-    await _firebaseAuth.createUserWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
-    await _firestore.collection('users').doc(currentUser?.uid).set({
-      'email': email,
-      'role': role,
-      // Tambahkan lebih banyak field yang ingin Anda simpan
-    });
-  }
-
   Future<void> signOut() async {
     await _firebaseAuth.signOut();
-  }
-
-  void toggleLoginStatus() {
-    isLogin = !isLogin;
-    notifyListeners();
   }
 }
