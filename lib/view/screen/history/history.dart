d0eb6eb2b5dabd8bd14b444ba2e79beb15c2f1bf
Chandra <ardhyachandrax@gmail.com>
2023-12-31 16:25:28 +0700

    finalling history page

--- lib/view/screen/history/history.dart
+++ lib/view/screen/history/history.dart
@@ -1,61 +1,85 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:point_of_sales/components/textstyle.dart';
 import 'package:point_of_sales/model/sales_model.dart';
 import 'package:point_of_sales/view/screen/history/history_detail_page.dart';
 
 class HistoryPage extends StatelessWidget {
   const HistoryPage({super.key});
-
   @override
   Widget build(BuildContext context) {
-    final user = FirebaseAuth.instance.currentUser;
-
     return Scaffold(
         appBar: AppBar(
-          title: const Text('History'),
+          title: Text(
+            'Riwayat Penjualan',
+            style: TextStyles.interBold.copyWith(
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: const Color(0xFFF87C47),
         ),
-        body: StreamBuilder<QuerySnapshot>(
+        body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
           stream: FirebaseFirestore.instance
               .collection('sales')
-              .doc(user!.uid)
-              .collection('history')
+              .doc(
+                  'history') // Gunakan 'history' sebagai ID dokumen untuk koleksi global
+              .collection(
+                  'transactions') // atau gunakan 'transactions' sebagai subkoleksi jika diinginkan
+              .orderBy('timestamp', descending: true)
               .snapshots(),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator();
+              return Center(child: const CircularProgressIndicator());
             } else if (snapshot.hasData) {
               List<SaleModel> sales = snapshot.data!.docs.map((doc) {
-                final data = doc.data() as Map<String, dynamic>;
+                final data = doc.data();
                 return SaleModel.fromMap(data);
               }).toList();
 
-              // Urutkan data penjualan berdasarkan timestamp secara ascending
-              sales.sort((a, b) => b.timestamp.compareTo(a.timestamp));
-
               if (sales.isEmpty) {
-                return const Center(
-                    child: Text('Belum ada riwayat penjualan.'));
+                return Center(
+                  child: Text(
+                    'Belum ada riwayat penjualan.',
+                    style: TextStyles.poppinsMedium.copyWith(fontSize: 16),
+                  ),
+                );
               }
-
               return ListView.builder(
                 itemCount: sales.length,
                 itemBuilder: (context, index) {
                   final sale = sales[index];
-                  return ListTile(
-                    title: Text(
-                        'Total: Rp.${NumberFormat.decimalPattern('id_ID').format(sale.total)}'),
-                    subtitle: Text(
-                        'Waktu: ${DateFormat('dd-MM-yyyy HH:mm').format(sale.timestamp)}'),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SaleDetailPage(sale: sale),
+                  return Container(
+                    decoration: BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(color: Colors.grey.shade300),
+                      ),
+                    ),
+                    child: ListTile(
+                      title: Text(
+                        'Total: Rp.${NumberFormat.decimalPattern('id_ID').format(sale.total)}',
+                        style: TextStyles.interBold.copyWith(
+                          fontSize: 16,
+                          color: Color(0xFF505050),
                         ),
-                      );
-                    },
+                      ),
+                      subtitle: Text(
+                        'Waktu: ${DateFormat('dd-MM-yyyy HH:mm').format(sale.timestamp)}',
+                        style: TextStyles.poppinsMedium.copyWith(
+                          fontSize: 14,
+                          color: Color(0xFFB6B6B6),
+                        ),
+                      ),
+                      trailing: const Icon(Icons.arrow_forward),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => SaleDetailPage(sale: sale),
+                          ),
+                        );
+                      },
+                    ),
                   );
                 },
               );
--- lib/view/screen/history/history_detail_page.dart
+++ lib/view/screen/history/history_detail_page.dart
@@ -1,35 +1,129 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:point_of_sales/components/textstyle.dart';
 import 'package:point_of_sales/model/sales_model.dart';
 
 class SaleDetailPage extends StatelessWidget {
   final SaleModel sale;
 
-  const SaleDetailPage({super.key, required this.sale});
+  const SaleDetailPage({Key? key, required this.sale}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sale Detail'),
+        iconTheme: IconThemeData(
+          color: Colors.white, //change your color here
+        ),
+        title: Text(
+          'Detail Riwayat Penjualan',
+          style: TextStyles.interBold.copyWith(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: const Color(0xFFF87C47),
       ),
       body: ListView(
+        physics: BouncingScrollPhysics(),
         children: [
           ListTile(
             title: Text(
-                'Total: Rp.${NumberFormat.decimalPattern('id_ID').format(sale.total)}'),
+              'Total Penjualan : Rp.${NumberFormat.decimalPattern('id_ID').format(sale.total)}',
+              style: TextStyles.interBold.copyWith(
+                fontSize: 18,
+                color: Color(0xFF505050),
+              ),
+            ),
             subtitle: Text(
-                'Waktu: ${DateFormat('dd-MM-yyyy HH:mm').format(sale.timestamp)}'),
+              'Waktu: ${DateFormat('dd-MM-yyyy HH:mm').format(sale.timestamp)}',
+              style: TextStyles.poppinsMedium.copyWith(
+                fontSize: 16,
+                color: Color(0xFFB6B6B6),
+              ),
+            ),
           ),
-          const Divider(),
-          const Text('Item yang dibeli:',
-              style: TextStyle(fontWeight: FontWeight.bold)),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 16,
+            ),
+            child: Text(
+              'Produk yang terjual :',
+              style: TextStyles.interBold.copyWith(
+                fontSize: 16,
+                color: Color(0xFF505050),
+              ),
+            ),
+          ),
           Column(
             children: sale.products.map((product) {
-              return ListTile(
-                title: Text(product.name),
-                subtitle: Text(
-                    'Harga: Rp.${NumberFormat.decimalPattern('id_ID').format(product.price)}'),
+              return Column(
+                children: [
+                  SizedBox(
+                    height: 5,
+                  ),
+                  ListTile(
+                    title: Row(
+                      children: [
+                        Container(
+                          width: 125,
+                          height: 111,
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.7),
+                                spreadRadius: 2,
+                                blurRadius: 10,
+                                offset: const Offset(0, 5),
+                              ),
+                            ],
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.network(
+                              product.file,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        Expanded(
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    product.name,
+                                    style: TextStyles.interBold
+                                        .copyWith(fontSize: 16),
+                                  ),
+                                  Text(
+                                    'Harga: Rp.${NumberFormat.decimalPattern('id_ID').format(product.price)}',
+                                    style: TextStyles.poppinsMedium.copyWith(
+                                        fontSize: 16, color: Color(0xFFB6B6B6)),
+                                  ),
+                                ],
+                              ),
+                              Spacer(), // Spacer to push 'x${sale.quantity[product.id]}' to the right
+                              Text(
+                                'x${sale.quantity[product.id]}',
+                                style:
+                                    TextStyles.interBold.copyWith(fontSize: 16),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Divider(
+                    thickness: 1,
+                    height: 10,
+                  ),
+                ],
               );
             }).toList(),
           ),
